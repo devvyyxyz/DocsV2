c077b5e86887de27b63124e26529518deafe0d98
DevvyyXYZ <devvyydev@protonmail.com>
2024-12-25 01:54:01 +0000

    updated links for github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,8 +22,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  organizationName: 'devyyxyzv', // Usually your GitHub org/user name.
+  projectName: 'docsV2', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -173,7 +173,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/devvyyxyz/docsv2/tree/main',
         },
         blog: false,
         // blog: {
